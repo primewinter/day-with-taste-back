ab24d3efddac721d1089739f79bcccaba972b7f1
primewinter <yuuni.cha@gmail.com>
2021-03-28 19:44:21 +0900

    feat:답변 제출 기능 추가

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,6 +14,36 @@ module.exports = {
            console.log('✅visit success');
            return res.status(200).json({success: true});
        });
+    },
+    submitAnswer: async(req, res, next) => {
+        console.log(req.body);
+        const answer = new Answer(req.body);
+        answer.regDt = new Date();
+        console.log('answer ::', answer);
+        answer.save((err, response) => {
+            if(err) {
+                console.log('❌submit err', err);
+                return res.json({success: false}, err);
+            }
+            console.log('✅submit success');
+
+
+            /*
+            Answer.find({answer: answer.answer}, (err, result) => {
+                if(err) {
+                    console.log('❌result err', err);
+                    return res.json({success: false}, err);
+                }
+
+                if(result.length==0) {
+                    // 일치율 낮춰서 재전송
+                }
+                console.log('✅result success');
+                return res.status(200).json(result);
+            })
+            */
+
+            return res.status(200).json(response);
+        });
     }
-    
 }
